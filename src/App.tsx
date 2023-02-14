07b4cb380618699d69be0b6a662d442e07472dc9
msdio <rlagofka1@gmail.com>
2023-02-14 17:10:00 +0900

    fix: mui 커스텀 테마 적용 안되던 현상 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Typography } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
 import Input from 'components/input';
+import { theme } from 'styles/theme';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <Typography variant='h1' color='primary.main'>
+        adfadfadf
+      </Typography>
       <Input></Input>
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#FFFBF2',
+      main: '#FFF8EA',
+      dark: '#DBC9AB',
+    },
+    success: {
+      light: '#58CE5C',
+      main: '#58CE5C',
+      dark: '#2C9442',
+    },
+    info: {
+      light: '#70C2FD',
+      main: '#1383F9',
+      dark: '#094BB3',
+    },
+    warning: {
+      light: '#F1F783',
+      main: '#D9E532',
+      dark: '#99A419',
+    },
+  },
+});
