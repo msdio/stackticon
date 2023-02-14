737dec448ab60596727fbeace48bc4bc855942de
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-14 15:43:39 +0900

    feat: Add simpleicons Img to know how to use cdn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Input from 'components/input';
-
 function App() {
   return (
     <>
+      <img height='32' width='32' src='https://cdn.simpleicons.org/react' />
+      <img height='32' width='32' src='https://cdn.simpleicons.org/javascript' />
+      <img height='32' width='32' src='https://cdn.simpleicons.org/spring' />
+
       <Input></Input>
+      <div></div>
     </>
   );
 }
