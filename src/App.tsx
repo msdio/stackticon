100f6e84651ec4f7b7b2201c2b26522f125e4b2a
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-14 14:47:22 +0900

    Feat: Add Input mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
+import Input from 'components/input';
+
 function App() {
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ backgroundColor: '#F7F3E9', width: '33vw', height: '100vh' }}></div>
-      <div style={{ backgroundColor: '#F0E3CA', width: '33vw', height: '100vh' }}></div>
-      <div style={{ backgroundColor: '#FFF8EA', width: '33vw', height: '100vh' }}></div>
-    </div>
+    <>
+      <Input></Input>
+    </>
   );
 }
 
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,5 +1,18 @@
+import { Autocomplete, TextField } from '@mui/material';
+import { Stacks } from 'mock/stacks';
+
 const Input = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Autocomplete
+        disablePortal
+        id='combo-box-demo'
+        options={Stacks}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label='Stack' />}
+      />
+    </div>
+  );
 };
 
 export default Input;
