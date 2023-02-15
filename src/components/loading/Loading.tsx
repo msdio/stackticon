3304eb63e573449bec40bd883b1d49d6590627cc
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-16 02:09:39 +0900

    feat: Add makeResult function to get image url
    html을 png로 변환한 후 firebase에서 url을 가져오는 역할.
    useEffect 시 한번에 비동기 처리를 하기 위해서 함수를 만들었다

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,11 +1,32 @@
 import Stacks from 'components/stacks';
-import { useRef } from 'react';
+import { toPng } from 'html-to-image';
+import { useEffect, useRef } from 'react';
+import { getCreatedImageUrl } from 'services/firebase/storage';
 
 const Loading = (props: any) => {
   const targetRef = useRef<HTMLDivElement>(null);
 
+  const getPngToImage = async () => {
+    if (targetRef.current === null) {
+      return;
+    }
+
+    const dataUrl = await toPng(targetRef.current, { cacheBust: true });
+    return dataUrl;
+  };
+
+  const makeResult = async () => {
+    const imageRef = await getPngToImage();
+    const resultUrl = await getCreatedImageUrl(imageRef!);
+    console.log(resultUrl);
+  };
+
+  useEffect(() => {
+    makeResult();
+  }, []);
   return (
     <div>
+      <div ref={targetRef}></div>
       <Stacks ref={targetRef} />
     </div>
   );
