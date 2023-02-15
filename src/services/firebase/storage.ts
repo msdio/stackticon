ac7361c3f001a400d8f523dc9b1f0a1b457984c0
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-16 02:12:55 +0900

    fix: Change upload Firebase function & Change makeImageRef function
    기존 Byte를 이용해 업로드하던 방식에서 string data_url을 이용해 업로드하는
    방식으로 변경.
    html to image에서 받아오는 데이터를 바로 사용할 수 있게 됨.
    
    이미지의 경로 덮어쓰기를 예방하기 위해 miliseconds를 이미지의 경로로 저장

--- src/services/firebase/storage.ts
+++ src/services/firebase/storage.ts
@@ -1,14 +1,16 @@
 import type { StorageReference } from 'firebase/storage';
+import { uploadString } from 'firebase/storage';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 import { storage } from './initialize';
 
-const makeImageRef = (imageRef: string) => {
-  const baseRef = ref(storage, `images/${imageRef}`);
+const makeImageRef = () => {
+  const miliseconds = new Date().getTime().toString();
+  const baseRef = ref(storage, `images/${miliseconds}`);
   return baseRef;
 };
 
-export const downloadImageURL = async (downLoadRef: StorageReference) => {
+const downloadImageURL = async (downLoadRef: StorageReference) => {
   const url = await getDownloadURL(downLoadRef);
   if (url) {
     return url;
@@ -16,14 +18,27 @@ export const downloadImageURL = async (downLoadRef: StorageReference) => {
   throw new Error('에러가 발생했습니다. 이미지 주소를 받아오지 못했습니다');
 };
 
-export const uploadImageByBytes = async (
-  imageRef: string,
-  image: Blob | Uint8Array | ArrayBuffer,
-) => {
-  const stackImageRef = makeImageRef(imageRef);
+const uploadImageByBytes = async (imageRef: string, image: Blob | Uint8Array | ArrayBuffer) => {
+  const stackImageRef = makeImageRef();
   const uploadRes = await uploadBytes(stackImageRef, image);
   if (uploadRes) {
-    downloadImageURL(uploadRes.ref);
+    return uploadRes;
   }
   throw new Error('에러가 발생했습니다. 이미지를 생성하지 못했습니다');
+};
+
+const uploadImageByString = async (imageRef: string) => {
+  const stackImageRef = makeImageRef();
+  const uploadRes = await uploadString(stackImageRef, imageRef, 'data_url');
+  if (uploadRes) {
+    return uploadRes;
+  }
+  throw new Error('에러가 발생했습니다. 이미지를 생성하지 못했습니다');
+};
+
+export const getCreatedImageUrl = async (imageRef: string) => {
+  const uploaded = await uploadImageByString(imageRef);
+  if (uploaded) {
+    return downloadImageURL(uploaded.ref);
+  }
 };
