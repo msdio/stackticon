af4eee7f6f0c2016b90c433ab0607f3c68cc31a3
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-15 10:22:55 +0900

    fix: Delete useless getIcon function

--- src/utils/getAllIconInfo.ts
+++ src/utils/getAllIconInfo.ts
@@ -2,15 +2,7 @@ import * as icons from 'simple-icons';
 
 import type { SimpleIcon } from 'simple-icons';
 
-const getAllIconInfo = () => {
-  if (icons) {
-    return icons;
-  }
-  throw new Error('에러가 발생했습니다. 아이콘 정보를 가져오지 못했습니다');
-};
-
 export const makeIconInfoArray = () => {
-  const iconObj = getAllIconInfo();
-  const iconArr: SimpleIcon[] = Object.values(iconObj);
+  const iconArr: SimpleIcon[] = Object.values(icons);
   return iconArr;
 };
