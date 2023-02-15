775505ed6f146c93b6d49ebdcdcc4aa97e9e6d16
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-15 13:27:44 +0900

    style: change import order to match eslint

--- src/utils/getAllIconInfo.ts
+++ src/utils/getAllIconInfo.ts
@@ -1,6 +1,5 @@
-import * as icons from 'simple-icons';
-
 import type { SimpleIcon } from 'simple-icons';
+import * as icons from 'simple-icons';
 
 export const makeIconInfoArray = () => {
   const iconArr: SimpleIcon[] = Object.values(icons);
