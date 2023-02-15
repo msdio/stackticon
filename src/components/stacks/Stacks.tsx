54c62a4a42fa0a49121e48268f9eb1cdd0a18d9d
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-16 02:02:44 +0900

    style: Add inline style in Stacks.tsx
    사용자에게 보여줄 결과 stack 배경(board) 스타일 작업

--- src/components/stacks/Stacks.tsx
+++ src/components/stacks/Stacks.tsx
@@ -4,7 +4,17 @@ import React from 'react';
 const Stacks = React.forwardRef<HTMLDivElement>((props: any, targetRef) => {
   const stackArr = ['Javascript', 'React', 'Typescript'];
   return (
-    <div ref={targetRef}>
+    <div
+      ref={targetRef}
+      style={{
+        margin: '50px',
+        padding: '50px',
+        display: 'grid',
+        gridTemplateRows: '1fr ',
+        gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr',
+        backgroundColor: 'gray',
+      }}
+    >
       {stackArr.map((stack) => (
         <Stack key={stack} stackName={stack}></Stack>
       ))}
