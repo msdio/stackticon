5326f2ac3601fca155e6074de308e4929d50f7ef
Juhyun <boochickenood@gmail.com>
2023-02-16 23:41:53 +0900

    refactor: Seperate pngToImage function
    Loading 컴포넌트의 역할과 html을 png로 바꾸는 로직을 분리하기 위해
    pngToImage 함수를 utils 하위에 분리

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,30 +1,21 @@
 import Stacks from 'components/stacks';
-import { toPng } from 'html-to-image';
 import Page404 from 'pages/page404';
 import { useEffect, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { getCreatedImageUrl } from 'services/firebase/storage';
+import { pngToImage } from 'utils/imageConverter';
 
 const Loading = () => {
   const navigate = useNavigate();
   const targetRef = useRef<HTMLDivElement>(null);
   const { state } = useLocation();
 
-  const getPngToImage = async () => {
-    if (targetRef.current === null) {
-      return;
-    }
-
-    const dataUrl = await toPng(targetRef.current, { cacheBust: true });
-    return dataUrl;
-  };
-
   const navigateToResult = (resultUrl: string) => {
     navigate('/result', { state: resultUrl });
   };
 
   const makeResult = async () => {
-    const imageRef = await getPngToImage();
+    const imageRef = await pngToImage(targetRef);
     const resultUrl = imageRef && (await getCreatedImageUrl(imageRef));
     if (resultUrl) {
       navigateToResult(resultUrl);
