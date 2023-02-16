f62e46edff63214187a63520341af6cd10b4baef
msdio <rlagofka1@gmail.com>
2023-02-17 02:17:46 +0900

    fix: github pages 배포를 위해 hash router로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { Box } from '@mui/material';
 import { ThemeProvider } from '@mui/system';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Router from 'Router';
 import { theme } from 'styles/theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename={process.env.REACT_APP_PUBLIC_URL}>
+      <HashRouter>
         <Box bgcolor='background.default' width='100vw' height='fit-content' minHeight='100vh'>
           <Router />
         </Box>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   );
 }
