78e6b89a75fd8ac4dd64ee517520aa8b934c29f2
Juhyun <boochickenood@gmail.com>
2023-02-17 01:43:06 +0900

    feat: Add result style & copy url & go to mainpage

--- src/pages/result/Result.tsx
+++ src/pages/result/Result.tsx
@@ -1,19 +1,52 @@
-import { useLocation } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { Button } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
+
+const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Img = styled.img`
+  width: 80vw;
+  max-width: 700px;
+  margin: 30px;
+`;
+const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const Result = () => {
   const { state } = useLocation();
-  console.log(state);
-  const clickCopyUrl = () => {
-    console.log('temp');
+  const clickCopyUrl = async () => {
+    await navigator.clipboard.writeText(state);
+    alert('url을 복사했습니다');
   };
   return (
-    <div>
-      <div>image</div>
+    <Container>
       <div>
-        <button onClick={clickCopyUrl}>url 복사하기</button>
-        <button>다시 만들기</button>
+        <Img src={state} alt='' />
       </div>
-    </div>
+      <Buttons>
+        <Button
+          style={{ marginRight: '10px' }}
+          onClick={clickCopyUrl}
+          variant='contained'
+          color='success'
+          startIcon='🍄'
+        >
+          url 복사하기
+        </Button>
+        <Link to='/' style={{ textDecoration: 'none' }}>
+          <Button style={{ marginLeft: '10px' }} variant='contained' color='info' endIcon='🏠'>
+            메인으로
+          </Button>
+        </Link>
+      </Buttons>
+    </Container>
   );
 };
 
