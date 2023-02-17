7cb424f4cf68f9df0257777da163d5ebd524768d
JuhyunLim <87933367+userJu@users.noreply.github.com>
2023-02-17 21:21:05 +0900

    feat: Add component redirecting Cute404 page when don't have useLocation state

---
+++ src/components/requireState/RequireState.tsx
@@ -0,0 +1,18 @@
+import Cute404 from 'pages/page404';
+import { useLocation } from 'react-router';
+
+interface RequireStateProps {
+  component: JSX.Element;
+}
+
+const RequireState = ({ component }: RequireStateProps) => {
+  const { state } = useLocation();
+
+  if (!state) {
+    return <Cute404 />;
+  }
+
+  return component;
+};
+
+export default RequireState;
