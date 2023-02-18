946485b9015c2fc89c1048dbdab3dc02e4939437
msdio <rlagofka1@naver.com>
2023-02-18 15:32:00 +0900

    fix: restore RequireState in router

--- src/components/requireState/RequireState.tsx
+++ src/components/requireState/RequireState.tsx
@@ -1,13 +1,16 @@
+import Cute404 from 'pages/page404';
+import { useLocation } from 'react-router-dom';
+
 interface RequireStateProps {
   component: JSX.Element;
 }
 
 const RequireState = ({ component }: RequireStateProps) => {
-  // const { state } = useLocation();
+  const { state } = useLocation();
 
-  // if (!state) {
-  //   return <Cute404 />;
-  // }
+  if (!state) {
+    return <Cute404 />;
+  }
 
   return component;
 };
