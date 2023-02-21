e99c334f6336563a902921310191e933d2ca1d3f
msdio <rlagofka1@gmail.com>
2023-02-21 14:13:34 +0900

    feat: attach google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { Box } from '@mui/material';
 import { ThemeProvider } from '@mui/system';
+import GoogleAnalyticsTracker from 'GoogleAnalyticsTracker';
 import { HashRouter } from 'react-router-dom';
 import Router from 'Router';
 import { theme } from 'styles/theme';
 
 function App() {
+  GoogleAnalyticsTracker();
+
   return (
     <ThemeProvider theme={theme}>
       <HashRouter>
