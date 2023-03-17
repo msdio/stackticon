2fe74ce6f2477c31d639f1dd682dd8c69addd9cd
msdio <rlagofka1@gmail.com>
2023-03-17 18:25:28 +0900

    feature: result page style

--- src/pages/result/Result.tsx
+++ src/pages/result/Result.tsx
@@ -1,7 +1,8 @@
 import styled from '@emotion/styled';
-import { Button, Typography } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import BackgroundCircle from 'components/BackgroundCircle';
 import ButtonOptions from 'components/button-options/ButtonOptions';
-import { APP_NAME } from 'constants/constants';
+import Header from 'components/header';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
@@ -21,6 +22,8 @@ const Img = styled.img`
 const Buttons = styled.div`
   display: flex;
   justify-content: center;
+
+  gap: 3.625rem;
 `;
 
 const Result = () => {
@@ -32,22 +35,65 @@ const Result = () => {
   });
 
   return (
-    <Container>
-      <Typography marginBottom={'10%'} fontWeight={'bold'} color={'#02343f'} variant='h1'>
-        {APP_NAME}
-      </Typography>
-      <div>
-        <Img src={state} alt='stackticon result' />
-      </div>
-      <Buttons>
-        <ButtonOptions url={state} />
-        <Link to='/' style={{ textDecoration: 'none' }}>
-          <Button style={{ marginLeft: '10px' }} variant='contained' color='info' endIcon='🏠'>
-            Return to main
-          </Button>
-        </Link>
-      </Buttons>
-    </Container>
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: '#f9f9f9',
+      }}
+    >
+      <Header />
+      <Container>
+        <img
+          src={require('../../assets/images/landing-label.png')}
+          alt='landing-label'
+          width='312px'
+          height='52px'
+          style={{
+            marginTop: '250px',
+            transform: 'translateX(-5px)',
+          }}
+        />
+        <div
+          style={{
+            marginTop: '50px',
+            marginBottom: '30px',
+          }}
+        >
+          <Img src={state} alt='stackticon result' />
+        </div>
+        <Buttons>
+          <ButtonOptions url={state} />
+          <Link to='/' style={{ textDecoration: 'none' }}>
+            <Button
+              variant='contained'
+              endIcon='🏠'
+              sx={{
+                width: '236px',
+                height: '56px',
+                backgroundColor: '#454A52',
+                borderRadius: '12px',
+                fontSize: '20px',
+                fontWeight: 'bold',
+
+                '&:hover': {
+                  backgroundColor: 'cg.1',
+                },
+              }}
+            >
+              Return to main
+            </Button>
+          </Link>
+        </Buttons>
+      </Container>
+
+      <Box position='absolute' left='-170px' top='-209px' zIndex='1'>
+        <BackgroundCircle />
+      </Box>
+      <Box position='absolute' right='-49px' bottom='-289px' zIndex='1'>
+        <BackgroundCircle />
+      </Box>
+    </div>
   );
 };
 
