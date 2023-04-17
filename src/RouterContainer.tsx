04d9d670ccc425bd6a7033e04c43f711cfb3ed70
msdio <rlagofka1@gmail.com>
2023-04-17 15:28:23 +0900

    feat: handle loading with suspense, fallback

--- src/RouterContainer.tsx
+++ src/RouterContainer.tsx
@@ -1,13 +1,18 @@
 import { Box } from '@mui/material';
+import { Suspense } from 'react';
 import Router from 'Router';
 import GoogleAnalyticsTracker from 'services/google-analytics/GoogleAnalyticsTracker';
+
+const loading = <div>Assembling UIs...</div>;
 
 const RouterContainer = () => {
   GoogleAnalyticsTracker();
 
   return (
     <Box bgcolor='background.default' width='100vw' height='fit-content' minHeight='100vh'>
-      <Router />
+      <Suspense fallback={loading}>
+        <Router />
+      </Suspense>
     </Box>
   );
 };
