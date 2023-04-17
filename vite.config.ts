660c05b2e992d8dbd7fc2edcc477c0fe9b25e55c
msdio <rlagofka1@gmail.com>
2023-04-17 15:07:47 +0900

    feat: create ite.config.ts file

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+import { dependencies } from './package.json';
+
+const renderChunks = (deps: Record<string, string>) => {
+  const chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-router-dom', 'react-dom', 'pretendard'].includes(key)) {
+      return;
+    }
+
+    if (key === 'firebase') {
+      chunks[key + '/app'] = [key + '/app'];
+      chunks[key + '/storage'] = [key + '/storage'];
+    } else {
+      chunks[key] = [key];
+    }
+  });
+
+  return chunks;
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-router-dom', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
+});
