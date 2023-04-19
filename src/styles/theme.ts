afdcdfd086b7629e8a27a4209727882466bb301a
msdio <rlagofka1@gmail.com>
2023-04-19 17:10:01 +0900

    refactor: seperate mui custom style files

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,52 +1,11 @@
 import { createTheme } from '@mui/material/styles';
 
+import { colors } from './color';
+import { typos } from './typo';
+
 export const theme = createTheme({
-  typography: {
-    fontFamily: [
-      'Pretendard Variable',
-      'Pretendard',
-      '-apple-system',
-      'BlinkMacSystemFont',
-      'system-ui',
-      'Roboto',
-      'Helvetica Neue',
-      'Segoe UI',
-      'Apple SD Gothic Neo',
-      'Noto Sans KR',
-      'Malgun Gothic',
-      'Apple Color Emoji',
-      'Segoe UI Emoji',
-      'Segoe UI Symbol',
-      'sans-serif',
-    ].join(','),
-  },
-  palette: {
-    p: {
-      '1': '#0066ff',
-      '2': '#3385ff',
-      '3': '#66a3ff',
-      '4': '#99c2ff',
-      '5': '#cce0ff',
-      '6': '#e6f0ff',
-      '7': '#f0f6ff',
-      '8': '#f5f9ff',
-    },
-
-    cg: {
-      '1': '#0A1320',
-      '2': '#1A2339',
-      '3': '#364261',
-      '4': '#4B597D',
-      '5': '#7886AA',
-      '6': '#B3BACD',
-    },
-
-    info: {
-      light: '#F9F9F9',
-      main: '#8A8A8A',
-      dark: '#444A52',
-    },
-  },
+  typography: typos,
+  palette: colors,
   components: {
     MuiAutocomplete: {
       styleOverrides: {
---
+++ src/styles/typo.ts
@@ -0,0 +1,19 @@
+export const typos = {
+  fontFamily: [
+    'Pretendard Variable',
+    'Pretendard',
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'system-ui',
+    'Roboto',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Apple SD Gothic Neo',
+    'Noto Sans KR',
+    'Malgun Gothic',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol',
+    'sans-serif',
+  ].join(','),
+};
