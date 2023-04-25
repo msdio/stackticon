b7e7bfaf3251f50794851fbfd33955aaec138ffb
msdio <rlagofka1@gmail.com>
2023-04-26 02:34:17 +0900

    feat: extract package.json path from repositorylocation

--- src/utils/resultUrl.ts
+++ src/utils/resultUrl.ts
@@ -7,3 +7,20 @@ export const makeUrlIntoBracket = (url: string) => {
 export const makeUrlIntoImgTag = (url: string) => {
   return `<a href="${GITHUB_LINK}"><img src="${url}" alt="stackticon" /></a>`;
 };
+
+export const getPackageJSONFromRepository = (url: string) => {
+  const PREFIX = 'https://raw.githubusercontent.com/';
+  const SUFFIX = '/package.json';
+
+  let repositoryLocation = url.replace('https://github.com/', '').replace('tree/', '');
+
+  if (repositoryLocation.at(-1) === '/') {
+    repositoryLocation = repositoryLocation.slice(0, -1);
+  }
+
+  if (!repositoryLocation.includes('/main')) {
+    repositoryLocation = repositoryLocation.concat('/main');
+  }
+
+  return PREFIX + repositoryLocation + SUFFIX;
+};
