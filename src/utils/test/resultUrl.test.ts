90e1a3ab970fa5ba125bdeeae4d853577f851f56
g2hhh2ee <g2hhh2ee@gmail.com>
2023-07-22 10:10:27 +0900

    Refactor URL Handling and Branch Detection (#120)
    Improved URL handling to differentiate between mono and multi-repositories.
    Removed trailing slashes at the end of the links for consistency.
    The changes allow access regardless of the branch name.

--- src/utils/test/resultUrl.test.ts
+++ src/utils/test/resultUrl.test.ts
@@ -26,7 +26,7 @@ describe('get package.json location from repository', () => {
   it('in multi repo, without slash at last', () => {
     const multiRepoUrl = 'https://github.com/msdio/stackticon';
     const packageFileInMultiRepo =
-      'https://raw.githubusercontent.com/msdio/stackticon/main/package.json';
+      'https://raw.githubusercontent.com/msdio/stackticon/HEAD/package.json';
 
     expect(getPackageJSONFromRepository(multiRepoUrl)).toBe(packageFileInMultiRepo);
   });
@@ -34,7 +34,7 @@ describe('get package.json location from repository', () => {
   it('in multi repo, with slash at last', () => {
     const multiRepoUrl = 'https://github.com/msdio/stackticon/';
     const packageFileInMultiRepo =
-      'https://raw.githubusercontent.com/msdio/stackticon/main/package.json';
+      'https://raw.githubusercontent.com/msdio/stackticon/HEAD/package.json';
 
     expect(getPackageJSONFromRepository(multiRepoUrl)).toBe(packageFileInMultiRepo);
   });
@@ -42,7 +42,7 @@ describe('get package.json location from repository', () => {
   it('in mono repo, without slash at last', () => {
     const monoRepoUrl = 'https://github.com/msdio/Tamago/tree/main/client';
     const packageFileInMonoRepo =
-      'https://raw.githubusercontent.com/msdio/Tamago/main/client/package.json';
+      'https://raw.githubusercontent.com/msdio/Tamago/HEAD/client/package.json';
 
     expect(getPackageJSONFromRepository(monoRepoUrl)).toBe(packageFileInMonoRepo);
   });
@@ -50,7 +50,7 @@ describe('get package.json location from repository', () => {
   it('in mono repo, with slash at last', () => {
     const monoRepoUrl = 'https://github.com/msdio/Tamago/tree/main/client/';
     const packageFileInMonoRepo =
-      'https://raw.githubusercontent.com/msdio/Tamago/main/client/package.json';
+      'https://raw.githubusercontent.com/msdio/Tamago/HEAD/client/package.json';
 
     expect(getPackageJSONFromRepository(monoRepoUrl)).toBe(packageFileInMonoRepo);
   });
