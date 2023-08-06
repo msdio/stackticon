8e00624e355040a58ffa317ae11a11c6d4b8576f
msdio <rlagofka1@gmail.com>
2023-08-06 12:56:34 +0900

    refactor: replace functions into new functions

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,5 +1,17 @@
+import { allIconNames } from './allIconInfo';
+
 export const extractUniqueElements = (skills: string[]) => {
   const uniqueArray = new Set(skills);
 
   return [...uniqueArray];
+};
+
+export const filterExisitingStacks = (array: string[]) => {
+  const names = allIconNames();
+
+  return array.filter((el) => names.has(el.replaceAll('-', '').toLowerCase()));
+};
+
+export const makeIntoSlugs = (stk: string) => {
+  return stk.replaceAll('-', '').toLowerCase();
 };
